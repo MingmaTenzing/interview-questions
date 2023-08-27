d1929ced635e5d694aef70a5d5ccd26ed3057455
MingmaTenzing <pasangsherpa689@gmail.com>
2023-08-27 15:51:35 +0800

    valid paranthesis

--- twosum.js
+++ twosum.js
@@ -1,18 +1,17 @@
-const nums = [0,1,2,9,4];
-const target = 3;
-const twosumArr = [];
-
-function twosum() { 
-
-    for (let i=0; i<nums.length; i++) {
- 
-    for (let j=i+1; j<nums.length; j++) {
-        if (nums[j] + nums[i] == target) {
-            twosumArr.push(nums[i], nums[j])
-            console.log(twosumArr)
+function TwoSum() {
+    const sum = 5;
+    const array = [10, 2, 3, 4, 5, 6];
+    const dummyarrray = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] + array[i] == sum) {
+          dummyarrray.push(array[j], array[i]);
+          console.log(dummyarrray);
         }
+      }
     }
-}
-}
-
-twosum();
+  }
+  
+  TwoSum();
+  
