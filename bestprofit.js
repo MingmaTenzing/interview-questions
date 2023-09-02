058120520ff182727d35df455d007d38e92ec611
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-02 11:35:54 +0800

    solved the bestprofit solution by myself

---
+++ bestprofit.js
@@ -0,0 +1,30 @@
+/// MY OWN WAY BY USING FOR LOOP AND CHARACTER MAP 
+function bestProfit(prices) {
+    const pricesMap = new Map();
+    let buyingPrice = [...prices].sort()[0];
+    let sellingPrice = 0;
+  
+    for (let i=0; i<prices.length; i++) {
+      pricesMap.set(prices[i], i +1 )
+    }
+    
+    for (let j=pricesMap.get(buyingPrice); j<prices.length; j++) {
+      if (pricesMap.get(buyingPrice) + 1 == prices.length) {
+        console.log('return')
+      }
+      if (prices[j] > sellingPrice) {
+        sellingPrice = prices[j]
+      }
+    }
+  
+    if (sellingPrice - buyingPrice <= 0) {
+      return console.log(0)
+    }
+    else return console.log(sellingPrice - buyingPrice)
+  
+    
+  }
+  
+  bestProfit([1,2 ]);
+
+  ///
