7a8a06be9e69526a768e4dbd058d528e295d0651
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-07 10:53:30 +0800

    two sorted sum solved by myself

--- playground.js
+++ playground.js
@@ -1,48 +1,20 @@
-/// whenever you are soliving a problem 
-///that uses pointer like L and R, 
-/// its better to use the while loop instead of for looop
-//// using while loop solution is at the bottom
-
 function twoSortedSum(arr, target) {
-  let l = 0;
-  let r = arr.length - 1;
+  l=0;
+  r= arr.length -1; 
 
-  for (let i = 0; i < arr.length; i++) {
-    sum = arr[l] + arr[r];
-
-    if (sum < target) {
-      l += 1;
-    } else if (sum > target) {
-      r -= 1;
-    } else {
-      return console.log([l, r]);
+  while (l < r) {
+    const sum = arr[l] + arr[r];
+    if (sum <target) {
+      l++
     }
+    else if (sum>target) {
+      r--
+    }
+    else  return console.log([l,r])
   }
-  
+
+
+
 }
 
-twoSortedSum([10,30], 40);
-
-
-
-//// using while LOOP 
-
-function twoSortedSumWhileLoop(arr,target) {
-
-let l=0;
-let r=arr.length -1; 
-
-while (l<r) {
-   const sum = arr[l] + arr[r]
-
-   if (sum < target) {
-    l++
-   }
-   else if (sum>target) {
-    r--
-   }
-   else return console.log([l,r]);
-}
-
-}
-twoSortedSumWhileLoop([10,30],40)
+twoSortedSum([2,4,7,8],9)
