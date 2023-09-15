1a0bf7885c80213ce40db40e9411b3f49cc421f8
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-15 10:17:24 +0800

    meeting times donebyself

--- playground.js
+++ playground.js
@@ -1,43 +1,14 @@
 function meetingTimes(times) {
-  times = times.sort((a, b) => a[0] - b[0]);
-  const startTimes = [];
-  const endTimes = [];
+  times = times.sort((a,b) => a-b)
 
-  console.log(times);
-  for (let i = 0; i < times.length; i++) {
-    startTimes[i] = times[i][0];
-  } 
-  for (let i = 0; i < times.length; i++) {
-    endTimes[i] = times[i][1];
+  for (let i=1; i<times.length; i++) {
+     let startTime = times[i][0];
+     let prevEndTime = times[i-1][1]
+     if (startTime < prevEndTime) {
+      return console.log (false)
+     }
   }
+  return console.log(true)
 
-  for (let i = 0; i < times.length; i++) {
-    if (endTimes[i] > startTimes[i + 1]) {
-       return console.log(false);
-
-    }
-    
 }
-console.log(true);
-}
-
-meetingTimes([
- [5,10], [8,15], [15,20],[20,25]
-]);
-
-
-// another way and faster way 
-
-const meetTimes = (times) => {
-    times.sort((a,b) => a[0] - b[0]);
-    for (let i=1; i<times.length; i++) {
-        const startTime = times[i][0];
-        const prevEndTime = times[i-1][1];
-
-        if (prevEndTime > startTime) {
-            return console.log(false);        }
-
-    }
-    return console.log(true)
-}
-meetTimes([ [5,10], [8,15], [15,20],[20,25]])
+meetingTimes([[10,20],[20,30],[10,20]])
