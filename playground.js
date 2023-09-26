d41aeb17b9b8d02999cc92e6492ee63820604cbf
MingmaTenzing <pasangsherpa689@gmail.com>
2023-09-26 09:50:47 +0800

    sumoftwo complete

--- playground.js
+++ playground.js
@@ -1,18 +1,17 @@
-function sumofTwo(nums1, nums2, target)  {
+function sumofTwo(nums1,nums2,target) {
 
   const set = new Set();
-  for ( elem of nums1) {
-     set.add(target - elem);
-  }
-  for ( num of nums2) {
-    if (set.has(num)) {
-      return console.log(true)
-    }
+  for (let elem of nums1) {
 
+    set.add(elem)
+  }
+  for (let elem of nums2) {
+    if (set.has(elem)) {
+      return console.log(true);
+    }
   }
   console.log(false)
-  
- }
- 
- sumofTwo([0,12,8],[3,1,4],2)
- 
+
+}
+
+sumofTwo([1,2,3],[5,2],8)
