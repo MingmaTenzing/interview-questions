da46f96cc8c08a91780c298780f29b9d02945660
MingmaTenzing <pasangsherpa689@gmail.com>
2023-10-01 08:38:15 +0800

    insertion sort complete

--- insertionSort.js
+++ insertionSort.js
@@ -1,21 +1,16 @@
 function insertionSort(nums) {
 
-    for (let i=1; i<nums.length; i++) {
-  
-      while (nums[i-1]> nums[i]) {
-        
-          let temp = nums[i-1];
-          nums[i-1] = nums[i];
-          nums[i] = temp;
-          i--;
-        }
-      }
-      
-      
-      console.log(nums)
-    }
-  
-  
-  
-  
-  insertionSort([9,7,5,2,8,4])
+for (let i=1; i<nums.length; i++) {
+  while (nums[i] < nums[i-1]){
+    let temp = nums[i];
+    nums[i] = nums[i-1];
+    nums[i-1] = temp;
+    i--
+  }
+
+}
+console.log(nums)
+
+}
+
+insertionSort([9,7,5,2,8,4] )
